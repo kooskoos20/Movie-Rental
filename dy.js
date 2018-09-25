495f875bb0514830cfee70db5ed16b35907c0441
kooskoos20 <omkarbirade20@gmail.com>
2018-09-25 17:39:57 +0530

    added project

---
+++ dy.js
@@ -0,0 +1,33 @@
+let total = 5;
+
+let reserved = 0;
+
+let track = {};
+
+//track.length = 6;
+
+let flag = -1;
+
+document.getElementById("count").innerHTML = total;
+document.getElementById("count2").innerHTML = reserved;
+
+function reserve(id) {
+  console.log(track.id);
+  console.log(id);
+
+  if (track[id] === undefined || track[id] === 0) {
+    reserved += 1;
+    document.getElementById("count2").innerHTML = reserved;
+    document.getElementById(id).innerHTML = "Return";
+
+    track[id] = 1;
+
+    flag = 1;
+  } else {
+    reserved -= 1;
+    document.getElementById("count2").innerHTML = reserved;
+    document.getElementById(id).innerHTML = "Reserve";
+    track[id] = 0;
+    flag = -1;
+  }
+}
